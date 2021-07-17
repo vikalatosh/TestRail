1c425186c760b21ef9d3e56cb5ec8b581e6e3ab1
vikalatosh <latosh.viktorija@mail.ru>
2021-07-17 20:52:01 +0300

    Added fakeValuesService

--- src/test/java/models/ProjectFactory.java
+++ src/test/java/models/ProjectFactory.java
@@ -1,15 +1,21 @@
 package models;
 
 import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.Locale;
 
 @Log4j2
 public class ProjectFactory {
     public static Project get() {
         log.info("Create new Project");
         Faker faker = new Faker();
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("en-GB"), new RandomService());
         return Project.builder()
-                .name(faker.app().name())
+                .name(faker.app().name() + fakeValuesService.regexify("[A-Z]{3}"))
                 .build();
     }
 }
